d6afc6ae74390f0b2176265e2acbe6e2a9ec641f
schubisu <robin.schubert@ghi-muenster.de>
2020-11-04 17:45:52 +0100

    creates vCalAddress items from email addresses, and vice versa

--- khal/ui/editor.py
+++ khal/ui/editor.py
@@ -432,6 +432,8 @@ class EventEditor(urwid.WidgetWrap):
             return True
         if get_wrapped_text(self.categories) != self.event.categories:
             return True
+        if get_wrapped_text(self.attendees) != self.event.attendees:
+            return True
         if self.startendeditor.changed or self.calendar_chooser.changed:
             return True
         if self.recurrenceeditor.changed:
@@ -444,6 +446,8 @@ class EventEditor(urwid.WidgetWrap):
         self.event.update_summary(get_wrapped_text(self.summary))
         self.event.update_description(get_wrapped_text(self.description))
         self.event.update_location(get_wrapped_text(self.location))
+        # TODO will overwrite attendee CN information (if any) on change
+        self.event.update_attendees(get_wrapped_text(self.attendees).split(','))
         self.event.update_categories(get_wrapped_text(self.categories).split(','))
 
         if self.startendeditor.changed:
